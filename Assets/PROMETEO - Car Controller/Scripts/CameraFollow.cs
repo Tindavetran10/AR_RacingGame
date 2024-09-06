7c562386b9e9c925765ad62793e68d654b0d6caf
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2024-09-06 19:38:58 +0700

    First Commit

---
+++ Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform carTransform;
+	[Range(1, 10)]
+	public float followSpeed = 2;
+	[Range(1, 10)]
+	public float lookSpeed = 5;
+
+	private Vector3 initialCameraPosition;
+	private Vector3 initialCarPosition;
+	private Vector3 absoluteInitCameraPosition;
+
+	private void Start(){
+		initialCameraPosition = gameObject.transform.position;
+		initialCarPosition = carTransform.position;
+		absoluteInitCameraPosition = initialCameraPosition - initialCarPosition;
+	}
+
+	private void FixedUpdate()
+	{
+		//Look at car
+		var lookDirection = new Vector3(carTransform.position.x, carTransform.position.y, carTransform.position.z) - transform.position;
+		var rot = Quaternion.LookRotation(lookDirection, Vector3.up);
+		transform.rotation = Quaternion.Lerp(transform.rotation, rot, lookSpeed * Time.deltaTime);
+
+		//Move to car
+		var targetPos = absoluteInitCameraPosition + carTransform.transform.position;
+		transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+	}
+}
